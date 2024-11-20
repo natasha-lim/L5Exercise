74c96905862ba21852cab5a5768a8f5b70709d83
natasha-lim <23014690@myrp.edu.sg>
2024-11-20 16:48:12 +0800

    Lesson 5 Exercises Updated

--- App.js
+++ App.js
@@ -1,20 +1,149 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// //=======================Exercise 1=========================
+// import React from 'react';
+// import {View, Text, FlatList, StyleSheet, TouchableOpacity, StatusBar} from 'react-native';
+//
+// const datasource = [
+//   {key:'a'},
+//   {key:'b'},
+//   {key:'c'},
+//   {key:'d'},
+//   {key:'e'},
+//   {key:'f'},
+//   {key:'g'},
+//   {key:'h'},
+//   {key:'i'},
+//   {key:'j'},
+//   {key:'k'},
+//   {key:'l'},
+//   {key:'m'},
+//   {key:'n'},
+//   {key:'o'},
+//   {key:'p'},
+//   {key:'q'},
+//   {key:'r'},
+//   {key:'s'},
+//   {key:'t'},
+//   {key:'u'},
+//   {key:'v'},
+//   {key:'w'},
+//   {key:'x'},
+//   {key:'y'},
+//   {key:'z'}
+// ];
+//
+// const styles = StyleSheet.create({
+//   opacityStyle: {
+//     borderWidth:1
+//   },
+//
+//   textStyle: {
+//     fontSize:15,
+//     margin:10,
+//     textAlign:"left"
+//   }
+//
+// })
+//
+// const renderItem = ({item}) => {
+//   return (
+//       <TouchableOpacity style={styles.opacityStyle}>
+//         <Text style={styles.textStyle}>{item.key}</Text>
+//       </TouchableOpacity>
+//   )
+// }
+//
+// const App = () => {
+//   return (
+//       <View style={{marginBottom: 20}}>
+//         <StatusBar hidden={true} />
+//         <FlatList data={datasource} renderItem={renderItem} />
+//       </View>
+//   );
+// };
+//
+// export default App;
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+//==========================Exercise 2=====================
+import React from 'react';
+import {View, Text, FlatList, StyleSheet, TouchableOpacity, StatusBar, SectionList} from 'react-native';
+
+const datasource = [
+  {data: [
+      {key:'a'},
+      {key:'e'},
+      {key:'i'},
+      {key:'o'},
+      {key:'u'},
+    ],
+  title:"Vowels", bgColour: 'yellow'},
+
+  {data: [
+      {key:'b'},
+      {key:'c'},
+      {key:'d'},
+      {key:'f'},
+      {key:'g'},
+      {key:'h'},
+      {key:'j'},
+      {key:'k'},
+      {key:'l'},
+      {key:'m'},
+      {key:'n'},
+      {key:'p'},
+      {key:'q'},
+      {key:'r'},
+      {key:'s'},
+      {key:'t'},
+      {key:'v'},
+      {key:'w'},
+      {key:'x'},
+      {key:'y'},
+      {key:'z'},
+    ],
+    title:"Consonants", bgColour: 'lightblue'},
+];
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  opacityStyle: {
+    borderWidth:1
   },
-});
+
+  textStyle: {
+    fontSize:15,
+    margin:10,
+    textAlign:"left"
+  },
+
+  headerText: {
+    fontSize: 20,
+    margin: 10,
+    textAlign: "center",
+    fontWeight: 'bold'
+  }
+
+})
+
+const renderItem = ({item}) => {
+  return (
+      <TouchableOpacity style={styles.opacityStyle}>
+        <Text style={styles.textStyle}>{item.key}</Text>
+      </TouchableOpacity>
+  )
+}
+
+const App = () => {
+  return (
+      <View style={{marginBottom: 20}}>
+        <StatusBar hidden={true} />
+        <SectionList sections={datasource} renderItem={renderItem}
+        renderSectionHeader={({section:{title, bgColour}}) => (
+            <Text style={[styles.headerText, {backgroundColor: bgColour}]}>{title}</Text>
+            )}
+            />
+      </View>
+  );
+};
+
+export default App;
+
+
